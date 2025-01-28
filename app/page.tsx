b4e1355b996651031fa9ad13202e16a5ca331d79
BLACKDRAGON03 <abdullahalsayef03@gmail.com>
2025-01-28 21:19:05 +0600

    added sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,57 @@
 "use client"
 
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import Header from "@/components/header";
 import Sidebar from "@/components/sidebar";
 import Slider from "@/components/slider";
 import Categories from "@/components/categories";
 import JustForYou from "@/components/justforyou";
 
+
 export default function Page() {
     const [isBarOpen, setIsBarOpen] = useState(false);
+    const [sidebarLeft, setSidebarLeft] = useState([0, 0]);
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+            if (window.innerWidth <= 1280) {
+                setSidebarLeft([0, 0])
+            } else {
+                setSidebarLeft([(window.innerWidth - (containerRef.current?.offsetWidth || 0)) / 2 - 4, (containerRef.current?.offsetWidth || 0) / 4 + 16])
+            }
+                
+        }, [])
+    
+        useEffect(() => {
+            let prevWidth = window.innerWidth;
+            const handleResize = () => {
+                const currWidth = window.innerWidth;
+    
+                if (prevWidth != currWidth) {
+                    prevWidth = currWidth;
+                    
+                    if (window.innerWidth <= 1280) {
+                        setSidebarLeft([0, 0])
+                    } else {
+                        setSidebarLeft([(window.innerWidth - (containerRef.current?.offsetWidth || 0)) / 2 - 4, (containerRef.current?.offsetWidth || 0) / 4 + 16])
+                    }
+                }
+                
+                
+            };
+    
+            window.addEventListener('resize', handleResize);
+    
+            return () => {
+                window.removeEventListener('resize', handleResize);
+            };
+        }, []);
+
     return (
-        <div className="container h-full mx-auto relative ">
+        <div ref={containerRef} className="container h-full mx-auto relative ">
             <Header isBarOpen={isBarOpen} setIsBarOpen={setIsBarOpen} />
-            <div className={`flex justify-end transtion-all duration-300  ${isBarOpen ? "" : ""}`}>
-                <Sidebar isBarOpen={isBarOpen} setIsBarOpen={setIsBarOpen} />
+            <div className={`flex justify-end transtion-all duration-300   ${isBarOpen ? "" : ""}`}>
+                <Sidebar left={sidebarLeft} isBarOpen={isBarOpen} setIsBarOpen={setIsBarOpen} />
                 <main className={`pt-[64px] sm:pt-[72px] lg:pt-[80px] xl:pt-[92px] px-2 lg:px-4  transition-all duration-300 overflow-x-hidden ${isBarOpen ? "xl:w-[calc(75%-8px)]" : "xl:w-full"}`}>
                     <Slider />
                     <Categories isBarOpen={isBarOpen} />
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
+import Link from "next/link";
+import { TiHome } from "react-icons/ti";
+import { HiMiniCog6Tooth } from "react-icons/hi2";
+import { FaShapes } from "react-icons/fa6";
+import { AiFillAppstore } from "react-icons/ai";
+import { FaShop } from "react-icons/fa6";
+import { MdLocalOffer } from "react-icons/md";
 
-export default function Sidebar({ isBarOpen, setIsBarOpen }: { isBarOpen: boolean, setIsBarOpen: React.Dispatch<React.SetStateAction<boolean>> }) {
+
+export default function Sidebar({ isBarOpen, setIsBarOpen, left }: { isBarOpen: boolean, setIsBarOpen: React.Dispatch<React.SetStateAction<boolean>>, left: number[] }) {
     const [isZUp, setIsZUp] = useState(false);
 
     useEffect(() => {
@@ -17,10 +25,33 @@ export default function Sidebar({ isBarOpen, setIsBarOpen }: { isBarOpen: boolea
         
     }, [isBarOpen])
     return (
-        <div onClick={() => isBarOpen && setIsBarOpen(false)} className={`fixed top-14 sm:top-16 xl:top-[75px] bottom-0 left-0   overflow-hidden  w-full  transition-all duration-300 ${isBarOpen ? " backdrop-blur-lg xl:backdrop-blur-none bg-black/[0] xl:bg-transparent  dark:xl:bg-transparent dark:bg-white/[0] " : " xl:w-0"} ${isZUp ? "z-40 xl:-z-50" : "-z-50"}`}>
-            <div className={`container mx-auto h-full py-2 px-2 lg:py-4 lg:px-4`}>
-                <div onClick={(e) => e.stopPropagation()} className={`bg-neutral-50 dark:bg-neutral-950 container rounded-md transition-all duration-300 overflow-hidden   h-full ${isBarOpen ? "w-2/3 md:w-1/2 lg:w-1/3 xl:z-30 xl:w-[calc(25%-8px)] " : "w-0"}`}>
-                    
+        <div style={{left: left[0], width: left[1]}} onClick={() => isBarOpen && setIsBarOpen(false)} className={`fixed  top-14 sm:top-16 xl:top-[76px] bottom-0   overflow-hidden max-lg:!w-full  transition-all duration-300 ${isBarOpen ? " backdrop-blur-lg xl:z-40 xl:backdrop-blur-none bg-black/[0] xl:bg-transparent  dark:xl:bg-transparent dark:bg-white/[0] " : " xl:!w-0"} ${isZUp ? "z-40 " : "-z-50"}`}>
+            <div className={`max-lg:container max-lg:mx-auto xl:absolute xl:w-full h-full py-2 px-2 lg:py-4 lg:px-4 `}>
+                <div onClick={(e) => e.stopPropagation()} className={`bg-neutral-50 dark:bg-neutral-950  rounded-md transition-all duration-300 overflow-hidden flex flex-col items-center justify-center xl:items-start   xl:z-40 h-full ${isBarOpen ? "w-2/3 p-6 md:w-1/2 lg:w-1/3  xl:w-full " : "w-0 "}`}>
+                    <Link href="/" className={`text-xl xl:text-3xl border-b-black/[0.1] font-semibold dark:border-b-white/[0.1] border-b flex w-full gap-3 items-center p-4 transition-all  ${isBarOpen ? "" : "opacity-0"}`}>
+                        <TiHome className="" /> 
+                        <div>Home</div>
+                    </Link>
+                    <Link href="/categories" className={`text-xl xl:text-3xl border-b-black/[0.1] font-semibold dark:border-b-white/[0.1] border-b flex w-full gap-3 items-center p-4 transition-all ${isBarOpen ? "" : "opacity-0"}`}>
+                        <FaShapes className="" /> 
+                        <div>Categories</div>
+                    </Link>
+                    <Link href="/products" className={`text-xl xl:text-3xl border-b-black/[0.1] font-semibold dark:border-b-white/[0.1] border-b flex w-full gap-3 items-center p-4 transition-all ${isBarOpen ? "" : "opacity-0"}`}>
+                        <AiFillAppstore className="" /> 
+                        <div>All products</div>
+                    </Link>
+                    <Link href="/special-offer" className={`text-xl xl:text-3xl border-b-black/[0.1] font-semibold dark:border-b-white/[0.1] border-b flex w-full gap-3 items-center p-4 transition-all ${isBarOpen ? "" : "opacity-0"}`}>
+                        <MdLocalOffer className="" /> 
+                        <div>Special offer</div>
+                    </Link>
+                    <Link href="/my-shop" className={`text-xl xl:text-3xl border-b-black/[0.1] font-semibold dark:border-b-white/[0.1] border-b flex w-full gap-3 items-center p-4 transition-all ${isBarOpen ? "" : "opacity-0"}`}>
+                        <FaShop className="" /> 
+                        <div>My shop</div>
+                    </Link>
+                    <Link href="/settings" className={`text-xl xl:text-3xl border-b-black/[0.1] font-semibold dark:border-b-white/[0.1]  flex w-full gap-3 items-center p-4 transition-all ${isBarOpen ? "" : "opacity-0"}`}>
+                        <HiMiniCog6Tooth />
+                        <div>Settings</div>
+                    </Link>
                 </div>
             </div>
         </div>
